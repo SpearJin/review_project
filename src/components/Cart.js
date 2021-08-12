05347acb1c098e0b1356dbd469d263e288581772
CHANGJIN PARK <changjinpark@CHANGJINui-MacBookPro.local>
2021-08-12 14:51:00 +0900

    Cart: state

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,39 @@
+import { useState } from "react";
+import styled from "styled-components";
+
 function Cart() {
+  const [quentity, setQuentity] = useState(5);
+  const price = quentity * 30000;
+
+  const CartComponent = styled.div`
+    padding: 10px;
+    font-size: 25px;
+  `;
+
+  const onIncrease = () => {
+    if (quentity < 9) {
+      setQuentity(quentity + 1);
+    }
+  };
+
+  const onDecrease = () => {
+    if (quentity > 0) {
+      setQuentity(quentity - 1);
+    }
+  };
+
   return (
-    <>
+    <CartComponent>
       <span>책상</span>
-      <button>
+      <button onClick={onDecrease}>
         <i className="fas fa-minus-square"></i>
       </button>
-      <span>5</span>
-      <button>
+      <span>{quentity}</span>
+      <button onClick={onIncrease}>
         <i className="fas fa-plus-square"></i>
       </button>
-      <span>150000원</span>
-    </>
+      <span>{price}</span>
+    </CartComponent>
   );
 }
 
